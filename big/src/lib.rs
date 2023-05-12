dc3895360fe117e0cb9f0efa4b0cf707c6e8fae2
Daniel Macovei <djmacovei@gmail.com>
2023-05-12 08:13:09 -0500

    make wasm available

--- big/src/lib.rs
+++ big/src/lib.rs
@@ -3,7 +3,7 @@ struct Component;
 impl bindings::Component for Component {
     /// Say hello!
     fn big_greet(name: String) -> String {
-      let greeting = bindings::greet(&name);
+      let greeting = bindings::hello_world(&name);
       let big = greeting.to_ascii_uppercase();
       big
     }
